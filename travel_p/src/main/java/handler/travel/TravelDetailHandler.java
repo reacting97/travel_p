009838a04873268468e852347bcbc12e62a6eb13
Juwon <KOSTA@DESKTOP-307CT33>
2023-04-28 15:32:15 +0900

    Weather VO 추가 TravelDetailHandelr 수정 travel_list.jsp 기능, css 수정 travel_detail.jsp DB연동, 날씨 연동, 카카오맵 API 연동, 주변 관광지 기능 추가

--- travel_p/src/main/java/handler/travel/TravelDetailHandler.java
+++ travel_p/src/main/java/handler/travel/TravelDetailHandler.java
@@ -6,6 +6,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,7 +24,8 @@ import food.FoodVO;
 import handler.Handler;
 import travel.TravelService;
 import travel.TravelVO;
-
+import weather.Weather;
+ 
 public class TravelDetailHandler implements Handler{
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) {
@@ -40,18 +42,56 @@ public class TravelDetailHandler implements Handler{
 		} else if(locs[0].endsWith("도")) {
 			loc = locs[1];
 		}
+		
 		ArrayList<FoodVO> list = foodService.selectByLoc(loc);
 		ArrayList<TravelVO> travelList = service.selectByLoc(loc);
+		Weather weather = null;
 		for(int i=0; i<travelList.size(); i++) {
 			if(travelList.get(i).getAddress().equals(travel.getAddress())) {
 				travelList.remove(i);
 			}
 		}
 		
+		Collections.shuffle(travelList);
+		int code = 0;
+		if(loc.startsWith("서울")) {
+			code = 109;
+		} else if(loc.startsWith("인천")) {			
+			code = 109;
+		} else if(loc.startsWith("대전")) {
+			code = 133;
+		} else if(loc.startsWith("광주")) {
+			code = 156;
+		} else if(loc.startsWith("대구")) {
+			code = 143;
+		} else if(loc.startsWith("울산")) {
+			code = 159;
+		} else if(loc.startsWith("부산")) {
+			code = 159;
+		} else if(loc.startsWith("제주") || loc.startsWith("서귀")) {
+			code = 184;
+		} else {
+			if(locs[0].equals("경기도")) {
+				code = 109;
+			} else if(locs[0].equals("강원도")) {
+				code = 105;
+			} else if(locs[0].equals("충청북도")) {
+				code = 131;
+			} else if(locs[0].equals("충청남도")) {
+				code = 133;
+			} else if(locs[0].equals("전라북도")) {
+				code = 146;
+			} else if(locs[0].equals("전라남도")) {
+				code = 156;
+			} else if(locs[0].equals("경상북도")) {
+				code = 143;
+			} else if(locs[0].equals("경상남도")) {
+				code = 159;
+			}
+		}
+		
 		String path = request.getServletContext().getRealPath("/WEB-INF/files/data.xml");
-		String urlStr = "http://api.data.go.kr/openapi/tn_pubr_public_trrsrt_api";
-				urlStr += "?serviceKey=hqbUzbZx%2BbQR6OgVCNvZDXGGWIVTWAIawDhN2Y9fbW6Pndu%2BrU9e1NaR9UpW7%2BPotKdwoD9cXlkHbSS7tzFRJQ%3D%3D";
-				urlStr += "&type=xml";
+		String urlStr = "https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId="+code;
 		try {
 			FileInputStream fis = new FileInputStream(path);
 //			URL url = new URL(urlStr);
@@ -69,6 +109,36 @@ public class TravelDetailHandler implements Handler{
 					longitude = element.getElementsByTagName("경도").item(0).getTextContent();
 				}
 			}
+			
+			URL url = new URL(urlStr);
+			URLConnection conn = url.openConnection();
+			DocumentBuilderFactory wFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder wBuilder = wFactory.newDocumentBuilder();
+			Document wDoc = wBuilder.parse(conn.getInputStream());
+			Element wRoot = wDoc.getDocumentElement();
+			NodeList loccations = wRoot.getElementsByTagName("location");
+			for(int i=0; i<loccations.getLength(); i++) {
+				Element loccation = (Element) loccations.item(i);
+				String city = loccation.getElementsByTagName("city").item(0).getTextContent();
+				if(city.substring(0, 2).equals(loc.substring(0, 2))) {
+					Element data = (Element)loccation.getElementsByTagName("data").item(1);
+					String dateTime = data.getElementsByTagName("tmEf").item(0).getTextContent();
+					String info = data.getElementsByTagName("wf").item(0).getTextContent();
+					if(info.startsWith("맑")) {
+						info = "맑음";
+					} else if(info.startsWith("흐") || info.startsWith("구름")) {
+						info = "흐림";
+					} else if(info.startsWith("비") || info.startsWith("흐리고 비")) {
+						info = "비";
+					}
+						
+					String tmn = data.getElementsByTagName("tmn").item(0).getTextContent();
+					String tmx = data.getElementsByTagName("tmx").item(0).getTextContent();
+					String rnst = data.getElementsByTagName("rnSt").item(0).getTextContent();//					NodeList datas = loccation.getElementsByTagName("data");
+					weather = new Weather(city, dateTime, info, tmn, tmx, rnst);
+				}
+			}
+			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -84,6 +154,7 @@ public class TravelDetailHandler implements Handler{
 		request.setAttribute("travelList", travelList);
 		request.setAttribute("latitude", latitude);
 		request.setAttribute("longtitude", longitude);
+		request.setAttribute("weather", weather);
 		return "/travel/travel_detail.jsp";
 	}
 }
